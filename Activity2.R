9f00e78d153fb54f83ab78014370b1c888d61ea8
Your User Name <hwang1@colgate.edu>
2022-01-31 12:11:17 -0500

    intro to matrix

--- Activity2.R
+++ Activity2.R
@@ -8,3 +8,8 @@ heights <- c(30,41,20,22)
 #convert to cm
 heights_cm <- heights*100
 heights_cm
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+Mat
