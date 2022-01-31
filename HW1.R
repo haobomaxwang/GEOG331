a7514cdd4e6c03a9a204418c170f1401f1607e32
Your User Name <hwang1@colgate.edu>
2022-01-31 12:11:44 -0500

    Finished question 1

---
+++ HW1.R
@@ -0,0 +1,10 @@
+# homework 1 
+
+
+# question 1: 
+# step 1: update the code
+# stpe 2: select the updated files under the top right window under "git" and click push
+# step 3: enter the user name and token to log into github and click push. 
+
+
+# question 2: 
