bc1067914d1946a77ecdd4c7e5fb64147d89f523
Your User Name <hwang1@colgate.edu>
2022-01-31 11:39:13 -0500

    Made some changes to activity 1 and answered question 1.

--- Activity1.R
+++ Activity1.R
@@ -1,2 +1,5 @@
 print("Hello World")
 print("test1")
+# step 1: update the code
+# stpe 2: select the updated files under the top right window under "git" and click push
+# step 3: enter the user name and token to log into github and click push. 
