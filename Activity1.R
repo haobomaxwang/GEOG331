5d73a169dd550efa1b395ac0c3bc989558fa8906
Your User Name <hwang1@colgate.edu>
2022-02-02 10:38:10 -0500

    Performed vector calculation with R.

--- Activity1.R
+++ Activity1.R
@@ -3,3 +3,11 @@ print("test1")
 # step 1: update the code
 # stpe 2: select the updated files under the top right window under "git" and click push
 # step 3: enter the user name and token to log into github and click push. 
+
+## Doing math with R
+a<- c(1,2,3,4,5)
+b<-c(6,7,8,9,10)
+c<-c(11,12,13,14,15)
+a+b
+b+c
+b*c
