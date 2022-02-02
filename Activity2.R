9cb27d0748c949a1d0972ce01e7fdb2679933caa
Your User Name <hwang1@colgate.edu>
2022-02-02 12:11:13 -0500

    Read in csv

--- Activity2.R
+++ Activity2.R
@@ -13,3 +13,25 @@ heights_cm
 #first argument is the vector of numbers to fill in the matrix
 Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
 Mat
+
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+Mat.bycol
+
+# what's in the first column of the two matrices
+Mat[ ,1]
+Mat.bycol[ ,1]
+# what's in the first role of the two matrices
+Mat[1, ]
+Mat.bycol[1, ]
+
+
+#set working directory 
+setwd("Z:\\students\\hwang\\DATA\\noaa_weather")
+#readin the data
+datW <- read.csv("Z:\\students\\hwang\\DATA\\noaa_weather\\2011124.csv",
+                 stringsAsFactors = T)
+
+#see the structure of the dataset
+str(datW)
