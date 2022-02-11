55277f680830aac71fcbcd3d2e8ecac17829f17b
Your User Name <hwang1@colgate.edu>
2022-02-11 12:15:04 -0500

    made all the histograms with normal distribution

--- Activity2.R
+++ Activity2.R
@@ -97,9 +97,100 @@ colnames(averageTemp) <- c("Name", "MAAT")
 datW$siteN <- as.numeric(datW$NAME)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #make a histogram for the first site in our levels
 hist(datW$TAVE[datW$siteN=="1"], freq = FALSE, 
      main = paste(levels(datW$NAME)[1]), xlab = "Average daily temperature",
      ylab = "Relative frequency", col="grey50",
      border="white")
+
+## 
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3",
+       lwd = 3)
+# standard deviation line below the mean with red (tomato3) color
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) - 
+         sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + 
+         sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+
+
+
+
+
+
+
+
+### to know the range of dataset 
+range(datW$TAVE[datW$siteN==1], na.rm = TRUE)
+
+
+#### thinking about normal distribution on the first plot
+
+h1 <- hist(datW$TAVE[datW$siteN == 1],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[1]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="grey50",
+           border="white")
+
+#the seq function generates a sequence of numbers that we can 
+## use to plot the normal across the range of temperature values 
+x.plot <- seq(-10,30, length.out = 100)
+
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-10,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+y.scaled <- (max(h1$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph  
+#the first two arguements are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+##
+## the blue dashed is the normal distribution using the 
+# mean and standard deviation calculated from the data. 
 
--- HW2.R
+++ HW2.R
@@ -53,3 +53,239 @@ sampleintegers_prime <- as.integer(c(2,3,5,7,11))
 # Can contain numbers, signs, and letters
 samplecharacter<- c("My", "name", "is", "bla", "bla...")
 
+
+#qustion 3: 
+
+hist(datW$TAVE[datW$siteN=="1"], freq = FALSE, 
+     main = paste(levels(datW$NAME)[1]), xlab = "Average daily temperature",
+     ylab = "Relative frequency", col="grey50",
+     border="white")
+
+
+# help(hist) 
+# frequency if true then it shows real frequency (or number of occurance)
+# if false then it describes relative frequency.
+
+# main is the main title of the graph. xlab is the x label and ylab
+# is y label.
+
+
+# col describes the color used for the histogram bars and border is 
+# the color of the border around the bars. 
+
+# help(paste) this function Takes multiple elements from the multiple vectors 
+# and concatenates them into a single element. 
+# in this case, it will paste out the characters refered by the code:
+# levels()datW$NAME)[1] which is aberdeen
+
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3",
+       lwd = 3)
+# standard deviation line below the mean with red (tomato3) color
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) - 
+         sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + 
+         sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+
+
+
+
+
+
+
+
+
+## to plot other graphs into a single plot
+par(mfrow=c(2,2))
+
+### plot siteN ==2 
+h2<- hist(datW$TAVE[datW$siteN=="2"], freq = FALSE, 
+     main = paste(levels(datW$NAME)[2]), xlab = "Average daily temperature",
+     ylab = "Relative frequency", col="grey50",
+     border="white")
+abline(v= mean(datW$TAVE[datW$siteN==2], na.rm = TRUE), 
+       col= "tomato3", lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==2], na.rm = TRUE)- 
+             sd(datW$TAVE[datW$siteN==2], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==2], na.rm = TRUE)+ 
+         sd(datW$TAVE[datW$siteN==2], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+
+x.plot2 <- seq(0,40, length.out = 100)
+
+y.plot2 <-  dnorm(seq(0,40, length.out = 100),
+                  mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE),
+                  sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE))
+
+y.scaled2 <- (max(h2$density)/max(y.plot2)) * y.plot2
+
+points(x.plot2,
+       y.scaled2, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## plot siteN ==3
+h3<- hist(datW$TAVE[datW$siteN=="3"], freq = FALSE, 
+     main = paste(levels(datW$NAME)[3]), xlab = "Average daily temperature",
+     ylab = "Relative frequency", col="black",
+     border="white")
+abline(v= mean(datW$TAVE[datW$siteN==3], na.rm = TRUE), 
+       col= "tomato3", lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==3], na.rm = TRUE)- 
+         sd(datW$TAVE[datW$siteN==3], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==3], na.rm = TRUE)+ 
+         sd(datW$TAVE[datW$siteN==3], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+
+
+
+
+
+
+x.plot3 <- seq(-40,40, length.out = 100)
+
+y.plot3 <-  dnorm(seq(-40,40, length.out = 100),
+                  mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE),
+                  sd(datW$TAVE[datW$siteN == 3],na.rm=TRUE))
+
+y.scaled3 <- (max(h3$density)/max(y.plot3)) * y.plot3
+
+points(x.plot3,
+       y.scaled3, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+
+
+
+
+## plot siteN ==4
+h4<- hist(datW$TAVE[datW$siteN=="4"], freq = FALSE, 
+     main = paste(levels(datW$NAME)[4]), xlab = "Average daily temperature",
+     ylab = "Relative frequency", col="aquamarine",
+     border="white")
+abline(v= mean(datW$TAVE[datW$siteN==4], na.rm = TRUE), 
+       col= "tomato3", lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==4], na.rm = TRUE)- 
+         sd(datW$TAVE[datW$siteN==4], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==4], na.rm = TRUE)+ 
+         sd(datW$TAVE[datW$siteN==4], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+
+
+
+x.plot4 <- seq(0,40, length.out = 100)
+
+y.plot4 <-  dnorm(seq(0,40, length.out = 100),
+                  mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE),
+                  sd(datW$TAVE[datW$siteN == 4],na.rm=TRUE))
+
+y.scaled4 <- (max(h4$density)/max(y.plot4)) * y.plot4
+
+points(x.plot4,
+       y.scaled4, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+
+
+
+## plot siteN ==5
+h5<- hist(datW$TAVE[datW$siteN=="5"], freq = FALSE, 
+     main = paste(levels(datW$NAME)[5]), xlab = "Average daily temperature",
+     ylab = "Relative frequency", col="cyan2",
+     border="white")
+abline(v= mean(datW$TAVE[datW$siteN==5], na.rm = TRUE), 
+       col= "tomato3", lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==5], na.rm = TRUE)- 
+         sd(datW$TAVE[datW$siteN==5], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+abline(v= mean(datW$TAVE[datW$siteN==5], na.rm = TRUE)+ 
+         sd(datW$TAVE[datW$siteN==5], na.rm = TRUE),
+       col= "tomato3",
+       lty = 3, 
+       lwd=3)
+
+
+
+
+
+x.plot5 <- seq(-30,30, length.out = 100)
+
+y.plot5 <-  dnorm(seq(-30,30, length.out = 100),
+                  mean(datW$TAVE[datW$siteN == 5],na.rm=TRUE),
+                  sd(datW$TAVE[datW$siteN == 5],na.rm=TRUE))
+
+y.scaled5 <- (max(h5$density)/max(y.plot5)) * y.plot5
+
+points(x.plot5,
+       y.scaled5, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
