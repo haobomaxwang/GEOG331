44532bdb058df5636d44f93a3792cfd89ca77cc7
haobomaxwang <hwang1@colgate.edu>
2022-03-04 15:48:26 -0500

    finished question 8

--- Activity3.R
+++ Activity3.R
@@ -222,3 +222,95 @@ assert(length(which(is.na(datW$air.tempQ2)))==length(which(is.na(datW$wind.speed
 
 plot(datW$DD, datW$wind.speedQ1, pch=19, type="b", xlab = "Day of Year",
      ylab="wind speed m/s")
+
+
+#question 7:
+
+# to see whether the soil temperature make sense, we 
+# can check the values against the air temperature 
+
+# it would be great if we can plot soil temperature and
+# air temperature on the same graph
+
+
+plot(datW$DD, datW$air.tempQ1,
+     xlab = "Day of Year", 
+     ylab = "air and soil temperature in C",
+     type="n")
+
+
+# plot air temperature  
+points(datW$DD, datW$air.tempQ1,
+       col= rgb(95/255,158/255,160/255,.5), pch=15)    
+
+#plot soil temperature     
+points(datW$DD, datW$soil.temp,
+       col= "tomato3", pch=19)
+
+
+# question 8
+# calculate the average air temperature (after removing skeptical data)
+mean(datW$air.tempQ2, na.rm = TRUE)
+# the answer is 20.00936 which is around 20.0 C
+
+# to know how many values in air. tempQ2 is NA
+length(which(is.na(datW$air.tempQ2)))
+# the answer is 13 . Therefore the number of entries calculated 
+# is 2118-13=2105
+
+# calculate the average wind speed (after removing skeptical data)
+mean(datW$wind.speedQ1, na.rm = TRUE)
+# the answer is 0.445263 which is around 0.45 M/s
+# because we removed the same rows from wind speed and
+# air temperature, the number of rows for those two 
+# columns should be the same. which is 2105
+
+# calculate the average for soil moisture and temperature 
+mean(datW$soil.moisture, na.rm = TRUE)
+# we have 0.1445495 which is around 0.145 m^3/m^3
+
+mean(datW$soil.temp, na.rm = TRUE)
+# we have 17.41777 which is around 17.4 C
+
+# we knew that there are 707 NAs in the soil measurements 
+# therefore the sample number should be 2118-707=1411
+
+
+# for the air temperature and wind speed data, 
+# we know that the measurement is reliable throughout the 
+# measuring period.There are only 13 skeptical values 
+# we changed in to NA
+which(is.na(datW$air.tempQ2))
+# from this code we know that the 13 values are 
+[1]   67  300  974  975  976  979  980 1658 1659 1660 1670 1671
+[13] 2039
+
+# meaning that they are not clustered after some specific date
+# so the active measuring period for the air T and wind Speed
+# data is just from 2018.6.12 to 2018.7.26
+
+# from the soil data graph we know that starting from some date
+# all the soil data were NAs
+# there are 707 NAs and in total 2118 observations 
+# so the 1412th row in the data sheet should be the first NA
+
+datW[1412, ]
+
+# we have 
+
+timestamp solar.radiation precipitation
+1412 7/11/2018 21:00               2             0
+lightning.acvitivy lightning.distance wind.dir wind.speed
+1412                  0                  0      272       0.21
+gust.speed air.temperature relative.humid
+1412       0.41            16.9            0.6
+atmospheric.pressure soil.moisture soil.temp doy hour
+1412                 96.4            NA        NA 192   21
+DD air.tempQ1 wind.speedQ1 air.tempQ2
+1412 192.875       16.9         0.21       16.9
+
+# so, until 2018.7.11 21:00 the soil data should be good
+
+# for total precipitation: 
+sum(datW$precipitation)
+# we get 177.828 mm
