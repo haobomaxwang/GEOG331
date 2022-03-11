619d2d805fd0bea5b30c0b7195a988761e247908
haobomaxwang <hwang1@colgate.edu>
2022-03-10 20:24:24 -0500

    finished coding question 3

--- Activity4.R
+++ Activity4.R
@@ -28,7 +28,7 @@ for(i in c(1,3)){
            *iris[ , i+1][iris$Species=="versicolor"])
 }
 
-# this is for sepel length * sepel width and petal length * petal width
+# this is for sepal length * sepal width and petal length * petal width
 # because the columns used in each calculation are next to each 
 # other, we can use i and i+1 while i has the value 1 or 3.
 
@@ -93,10 +93,10 @@ plot(iris$Sepal.Length,iris$Sepal.Width)
 ## Assign plot to a variable 
 plot1<- ggplot(data = iris1, 
        mapping = aes(x = Sepal.Length, y = Sepal.Width))
-# draw the plot
-plot1 +
+  
+## draw the plot
+plot1 + 
   geom_point()
-
 
 # note that 
 # # This is the correct syntax for adding layers
@@ -113,10 +113,44 @@ plot1 +
 
 #3b. make a scatter plot with ggplot and get rid of
 # busy grid lines
+plot1 + 
+  geom_point()+ 
+  
+# this will remove the background color
+theme_bw()+ 
+# this will remove the grid lines
+theme(panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank())
 
 
-#3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
-#    show species by color, and make the point size proportional to petal length
+#3c. make a scatter plot with ggplot, remove grid lines, 
+# add a title and axis labels, 
+# show species by color, 
+# and make the point size proportional to petal length
+
+
+
+plot1 + 
+  geom_point()+ 
+  
+  # this will remove the background color
+  theme_bw()+ 
+  # this will remove the grid lines
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+# title 
+ggtitle("Sepal Length vs Sepal Width")+
+# x label
+xlab("Sepal Length cm")+
+# y label
+ylab("Sepal Width cm")+
+
+# show species by color 
+aes(color = Species)+
+
+# point size porpotional to petal length 
+aes(size = Petal.Length)
+
 
 #####################################
 ##### Question: how did         #####
