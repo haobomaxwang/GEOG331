7d088fb76c6ec54a81f34dab4eeb6c0438561bfb
haobomaxwang <hwang1@colgate.edu>
2022-04-13 12:02:24 -0400

    in-class activity 4/13

--- In class activity raster data.R
+++ In class activity raster data.R
@@ -20,3 +20,28 @@ plotRGB(p, r=3, g=2, b=1,
         scale = 65535,
         stretch = "hist")
 help("plotRGB")
+
+## readin csv
+tree<- read.csv("Z:/students/hwang/DATA/siberia_stand_data.csv", 
+                header = T)
+
+## convert to vector 
+gtree<- vect(tree, geom = c("Long", "Lat"), "epsg:4326")
+
+## project the projected coordinate system to match rastor data
+
+gtree2<- project(gtree, p)
+
+## plot the tree points
+plot(gtree2, add=T, col= "red")
+
+# create a polygon from the extent of the points
+b<- as.lines(ext(gtree), "epsg:4326")
+
+# reproject the polygons to the same projection 
+b2<- project(b, crs(p))
+plot(b2, add=T)
+
+# buffer the extent by 200 m 
+b3<- buffer(b2, width=200)
+plot(b3, add=T)
