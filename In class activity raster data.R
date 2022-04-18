6f7b49d441a9acf155f864cf9bae145b29664541
haobomaxwang <hwang1@colgate.edu>
2022-04-18 12:11:47 -0400

    plotted ndvi vs canopy cover

--- In class activity raster data.R
+++ In class activity raster data.R
@@ -45,3 +45,47 @@ plot(b2, add=T)
 # buffer the extent by 200 m 
 b3<- buffer(b2, width=200)
 plot(b3, add=T)
+
+# use this to crop the raster layer so we can see just
+# the area of interest
+p2<- crop(p,b3, overwrite=T,
+          filename= "20190706_SR_crop.tif")
+
+## plot the newly cropped raster
+plotRGB(p2,r=3, g=2, b=1,
+        scale = 65535,
+        stretch = "lin")
+
+# show the points again
+points(gtree2, col= "blue", cex=gtree2$cc.pct/50)
+
+
+# create a plot of the ndvi map with sample points on top
+# calculate ndvi
+ndvi<- (p2[[4]]-p2[[3]])/(p2[[4]]+p2[[3]])
+names(ndvi)<- "ndvi"
+ndvi
+
+
+## make a png file 
+help(png)
+
+png(filename = "ndvi_map.png",
+    width = 6, height = 4, units = "in", 
+    res = 300)
+
+plot(ndvi)
+points(gtree2, col= "blue", cex=gtree2$cc.pct/50)
+
+dev.off()
+
+# extract NDVI values for each point
+help(extract)
+nt<- terra::extract(ndvi, gtree2, fun= mean, 
+                    method= "bilinear") #four nearest cells
+
+nt
+
+plot(nt$ndvi, gtree2$cc.pct, 
+     pch=16, col= "blue",
+     xlim= c(0,1))
